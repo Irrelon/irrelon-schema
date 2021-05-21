0bcf672a0d3f8886c324728011baf607e8759b90
robevans <rob@irrelon.com>
2021-05-21 12:34:46 +0100

    Spread debug logging arguments

--- src/Schema.js
+++ src/Schema.js
@@ -27,31 +27,31 @@ class Schema {
 	constructor (definition, options = {}) {
 		this.definition(definition);
 		this._options = options;
-		
+
 		if (options.name) {
 			this.name(options.name);
 		}
-		
+
 		if (options.primaryKey) {
 			this.primaryKey(options.primaryKey);
 		}
-		
+
 		if (options.helpers) {
 			this.helpers(options.helpers);
 		}
 	}
-	
+
 	/**
 	 * Does a console.log() if options.debug is set to true.
 	 * @param {String} msg The message to log.
 	 * @returns {*} Nothing
 	 */
-	debugLog (msg) {
+	debugLog (...msg) {
 		if (this._options.debug) {
-			console.log(`applyModels :: ${msg}`);
+			console.log(`applyModels :: `, ...msg);
 		}
 	}
-	
+
 	/**
 	 * Get or set helpers defined on this schema instance.
 	 * @param {Object} obj An object containing helper functions.
@@ -62,11 +62,11 @@ class Schema {
 		if (obj === undefined) {
 			return this._helpers;
 		}
-		
+
 		this._helpers = obj;
 		return this;
 	}
-	
+
 	/**
 	 * Call a helper by its id with the passed model and any
 	 * other arguments you wish to pass.
@@ -79,7 +79,7 @@ class Schema {
 	helper (id, model, ...args) {
 		return this._helpers[id](model, ...args);
 	}
-	
+
 	/**
 	 * Gets / sets the schema name.
 	 * @param {String=} val The name to set if provided.
@@ -90,11 +90,11 @@ class Schema {
 		if (val === undefined) {
 			return this._name;
 		}
-		
+
 		this._name = val;
 		return this;
 	}
-	
+
 	/**
 	 * Gets / sets the schema primary key.
 	 * @param {String=} val The primary key to set if provided.
@@ -105,11 +105,11 @@ class Schema {
 		if (val === undefined) {
 			return this._primaryKey;
 		}
-		
+
 		this._primaryKey = val;
 		return this;
 	}
-	
+
 	/**
 	 * Gets / sets the schema definition.
 	 * @param {Object=} val The definition to set if provided.
@@ -120,14 +120,14 @@ class Schema {
 		if (val === undefined) {
 			return this._definition;
 		}
-		
+
 		this._definition = val;
-		
+
 		// Convert definition into normalised version
 		this.normalised(normalise(this));
 		return this;
 	}
-	
+
 	/**
 	 * Gets / sets the normalised schema definition object. Normalised
 	 * schema definitions are definitions that have been transformed so
@@ -143,11 +143,11 @@ class Schema {
 		if (val === undefined) {
 			return this._normalised;
 		}
-		
+
 		this._normalised = val;
 		return this;
 	}
-	
+
 	/**
 	 * NOT YET WRITTEN, DO NOT USE
 	 * Casts a model to the correct types if possible.
@@ -159,23 +159,23 @@ class Schema {
 	cast = (model) => {
 		return model;
 	};
-	
+
 	add = (obj) => {
 		if (!obj) return;
-		
+
 		// Take the new definition and add it to our existing one
 		this._definition = {...this._definition, ...obj};
-		
+
 		// Convert definition into normalised version
 		this.normalised(normalise(this));
 		return this;
 	};
-	
+
 	simplify = () => {
 		// Use the simplify() function defined *outside* the class
 		return simplify(this.normalised());
 	};
-	
+
 	/**
 	 * Checks if the passed model is valid or not and returns
 	 * a boolean true or false.
@@ -188,7 +188,7 @@ class Schema {
 	isValid = (model, options) => {
 		return this.validate(model, options).valid;
 	};
-	
+
 	/**
 	 * Validates model data against the schema.
 	 * @param {Object|Array} model The model to validate against the
@@ -207,21 +207,21 @@ class Schema {
 			options = currentPath;
 			currentPath = undefined;
 		}
-		
+
 		const schemaDefinition = this.normalised();
-		
+
 		if (model !== undefined && typeof model !== "object") {
 			// We are validating a model against a schema which means
 			// the model MUST be an object, reject this as a validation failure
 			const expectedSchema = simplify(schemaDefinition);
-			
+
 			return {
 				"valid": false,
 				"path": currentPath,
 				"reason": `Schema violation, "${currentPath}" expects an object that conforms to the schema ${JSON.stringify(expectedSchema)} and cannot be set to value ${String(JSON.stringify(model)).substr(0, 10)} of type ${getTypeName(model)}`
 			};
 		}
-		
+
 		// Now check for any fields in the model that
 		// don't exist in the schema
 		for (const i in model) {
@@ -230,7 +230,7 @@ class Schema {
 					// Found a field that should not exist in the
 					// model because it is not defined in the schema
 					const currentFullPath = pathJoin(currentPath, i);
-					
+
 					return {
 						"valid": false,
 						"path": currentFullPath,
@@ -239,10 +239,10 @@ class Schema {
 				}
 			}
 		}
-		
+
 		return this._validate(schemaDefinition, model, options.originalModel || model, currentPath, options);
 	};
-	
+
 	_validate = (currentSchema, currentModel, originalModel, parentPath = "", options = {"throwOnFail": false}) => {
 		if (currentSchema instanceof Schema) {
 			// Get the definition for this schema
@@ -251,33 +251,33 @@ class Schema {
 				"throwOnFail": options.throwOnFail
 			});
 		}
-		
+
 		if (isPrimitive(currentSchema)) {
 			const validator = getTypeValidator(currentSchema, {required: false}, (type) => {
 				if (type instanceof Schema) {
 					return type.validate;
 				}
 			});
-			
+
 			// Validate the model value against the schema type
 			return validator(currentModel, parentPath, {
 				originalModel,
 				"throwOnFail": options.throwOnFail
 			});
 		}
-		
+
 		const fieldsChecked = [];
-		
+
 		for (const i in currentSchema) {
 			if (currentSchema.hasOwnProperty(i)) {
 				fieldsChecked.push(i);
 				const currentFullPath = pathJoin(parentPath, i);
-				
+
 				// Get the value from the schema and the model for the
 				// current path key
 				const schemaFieldValue = pathGet(currentSchema, i);
 				let modelFieldValue = pathGet(currentModel, i);
-				
+
 				// Apply any defaults if required
 				if (modelFieldValue === undefined) {
 					if (schemaFieldValue.default !== undefined) {
@@ -285,33 +285,33 @@ class Schema {
 						pathSet(currentModel, i, schemaFieldValue.default);
 					}
 				}
-				
+
 				if (schemaFieldValue.transform) {
 					// We have a transform function, execute it and set the response value
 					// to the model value at this path
 					pathSet(currentModel, i, schemaFieldValue.transform(pathGet(currentModel, i), pathGet(originalModel, parentPath), originalModel));
 				}
-				
+
 				// Get updated model field value
 				modelFieldValue = pathGet(currentModel, i);
-				
+
 				// Get the validator for this field
 				const validator = getTypeValidator(schemaFieldValue.type, schemaFieldValue, (type) => {
 					if (type instanceof Schema) {
 						return type.validate;
 					}
 				});
-				
+
 				// Validate the model value against the schema type
 				const result = validator(modelFieldValue, currentFullPath, schemaFieldValue, {
 					originalModel,
 					"throwOnFail": options.throwOnFail
 				});
-				
+
 				if (!result.valid) {
 					return result;
 				}
-				
+
 				// Now we need to handle recursive behaviour based on types
 				if (schemaFieldValue.type === Array) {
 					if (modelFieldValue === undefined && schemaFieldValue.elementRequired === true) {
@@ -321,14 +321,14 @@ class Schema {
 							"reason": "At least on array element is required by schema"
 						};
 					}
-					
+
 					if (modelFieldValue !== undefined) {
 						// The schema value is an array instance so we use the first element
 						// of the schema value array as the type to validate all model array
 						// elements against
 						for (let arrIndex = 0; arrIndex < modelFieldValue.length; arrIndex++) {
 							const result = this._validate(schemaFieldValue.elementType, modelFieldValue[arrIndex], originalModel, pathJoin(currentFullPath, arrIndex));
-							
+
 							if (!result.valid) {
 								return result;
 							}
@@ -337,12 +337,12 @@ class Schema {
 				}
 			}
 		}
-		
+
 		return {
 			"valid": true
 		};
 	};
-	
+
 	/**
 	 * Converts the schema definition to a flat object with keys
 	 * representing each schema path and values representing the
@@ -351,16 +351,16 @@ class Schema {
 	 */
 	flattenValues (parentPath = "", values = {}, visited = []) {
 		const def = this.normalised();
-		
+
 		// Add our own schema instance to the visited array
 		// so we don't recurse if we find it nested in our
 		// own definition (we don't want infinite recursion!).
 		visited.push(this);
-		
+
 		Object.entries(def).map(([key, val]) => {
 			flatten(val, compoundKey(parentPath, key), values, visited);
 		});
-		
+
 		return values;
 	}
 }
@@ -375,7 +375,7 @@ const compoundKey = (...keys) => {
 			finalKey += `.${key}`;
 			return finalKey;
 		}
-		
+
 		return key;
 	}, "");
 };
@@ -388,14 +388,14 @@ const isSchemaInstance = (val) => {
 	if (!val) return false;
 	if (!val.constructor) return false;
 	if (val.constructor.name !== "Schema") return false;
-	
+
 	return true;
 };
 
 const isObjectDefinition = (val) => {
 	if (typeof val !== "object") return false;
 	if (!val.type) return false;
-	
+
 	return true;
 };
 
@@ -404,12 +404,12 @@ const validateObjectDefinition = (fieldData, key) => {
 	if (fieldData.transform !== undefined && typeof fieldData.transform !== "function") {
 		throw new Error(`Schema definition invalid at path "${key}": The "transform" field must be a function.`);
 	}
-	
+
 	// If we have a default value, make sure we don't also have required:true
 	if (fieldData.default !== undefined && fieldData.required === true) {
 		throw new Error(`Schema definition invalid at path "${key}": Cannot specify both required:true AND a default since default values are only applied when a field is not explicitly specified.`);
 	}
-	
+
 	// If we have a default value, make sure it validates against the field type
 	if (fieldData.default !== undefined) {
 		// Validate the default
@@ -418,9 +418,9 @@ const validateObjectDefinition = (fieldData, key) => {
 				return type.validate;
 			}
 		});
-		
+
 		const validatorResult = validator(fieldData.default, key, fieldData);
-		
+
 		if (!validatorResult.valid) {
 			throw new Error(`Schema definition invalid at path "${key}": Cannot specify a default value of type ${validatorResult.actualType} when the field type is ${validatorResult.expectedType}.`);
 		}
@@ -433,31 +433,31 @@ const flatten = (def, parentPath = "", values = {}, visited = []) => {
 		values[parentPath] = def;
 		return values;
 	}
-	
+
 	if (isPrimitive(def)) {
 		values[parentPath] = def;
 		return values;
 	}
-	
+
 	if (isSchemaInstance(def)) {
 		values[parentPath] = def;
 		return values;
 	}
-	
+
 	if (!isObjectDefinition(def)) {
 		// We need to throw an error as we don't know what to do now!
 		throw new Error("Unable to flatten object whose signature we don't understand!");
 	}
-	
+
 	// At this point we know the def is an object definition
 	const {type, elementType} = def;
-	
+
 	// Check for array instance
 	if (type === Array) {
 		values[parentPath] = Array;
-		
+
 		const valueKey = compoundKey(parentPath, "$");
-		
+
 		if (elementType) {
 			// Recurse into the array data
 			flatten(normaliseField(elementType, valueKey, visited), valueKey, values, visited);
@@ -465,39 +465,39 @@ const flatten = (def, parentPath = "", values = {}, visited = []) => {
 			// Assign a Schema.Any to the element type
 			values[valueKey] = Schema.Any;
 		}
-		
+
 		return values;
 	}
-	
+
 	if (isCustomType(type)) {
 		values[parentPath] = type;
 		return values;
 	}
-	
+
 	if (isPrimitive(type)) {
 		values[parentPath] = type;
 		return values;
 	}
-	
+
 	if (isSchemaPrimitive(type)) {
 		values[parentPath] = type;
 		return values;
 	}
-	
+
 	if (isSchemaInstance(type)) {
 		values[parentPath] = type;
-		
+
 		// Check if we have already added this schema type to the flattened object
 		if (visited.indexOf(type) > -1) {
 			return values;
 		}
-		
+
 		visited.push(type);
-		
+
 		type.flattenValues(parentPath, values, visited);
 		return values;
 	}
-	
+
 	return values;
 };
 
@@ -505,15 +505,15 @@ const getTypePrimitiveName = (val) => {
 	if (isPrimitive(val)) {
 		return val.name;
 	}
-	
+
 	if (val === null) {
 		return "null";
 	}
-	
+
 	if (val === undefined) {
 		return "undefined";
 	}
-	
+
 	return val.constructor.name;
 };
 
@@ -528,11 +528,11 @@ const normalise = (def) => {
 	const values = {};
 	const visited = [];
 	const schemaDefinition = def.definition();
-	
+
 	Object.entries(schemaDefinition).map(([key, val]) => {
 		values[key] = normaliseField(val, key, visited);
 	});
-	
+
 	return values;
 };
 
@@ -543,7 +543,7 @@ const normaliseField = (field, key, visited = []) => {
 			"required": false
 		};
 	}
-	
+
 	if (isPrimitive(field)) {
 		if (field === Array) {
 			return {
@@ -552,48 +552,48 @@ const normaliseField = (field, key, visited = []) => {
 				"elementType": Schema.Any
 			};
 		}
-		
+
 		return {
 			"type": field,
 			"required": false
 		};
 	}
-	
+
 	if (isSchemaPrimitive(field)) {
 		return {
 			"type": field,
 			"required": false
 		};
 	}
-	
+
 	if (isSchemaInstance(field)) {
 		// Check if we have already added this schema type to the flattened object
 		if (visited.indexOf(field) > -1) {
 			return undefined;
 		}
-		
+
 		visited.push(field);
-		
+
 		return {
 			"type": field,
 			"required": false
 		};
 	}
-	
+
 	if (field instanceof Array) {
 		const elementType = field[0] ? normaliseField(field[0], compoundKey(key, "$")) : Schema.Any;
-		
+
 		return {
 			"type": Array,
 			elementType,
 			"required": false
 		};
 	}
-	
+
 	if (isObjectDefinition(field)) {
 		// Check object definition is valid
 		validateObjectDefinition(field, key);
-		
+
 		if (field.type === Array) {
 			if (field.elementType) {
 				// Normalise the element type
@@ -603,14 +603,14 @@ const normaliseField = (field, key, visited = []) => {
 				field.elementType = Schema.Any;
 			}
 		}
-		
+
 		// Make sure the field has default settings
 		return {
 			"required": false,
 			...field
 		};
 	}
-	
+
 	// We don't know how to handle this field information!
 	throw new Error(`Unable to determine what to do with the field data at "${key}". We don't recognise the format! ${JSON.stringify(field)}`);
 };
